130c74ae9b6e0c2a80f4a5b18d57b605c93b0c4e
Thompson solomon <thompsonsolomon123@gmail.com>
2025-04-08 08:21:06 +0100

    Delete node_modules directory

--- node_modules/react-paystack/example/src/dist/paystack-context.d.ts
+++
@@ -1,9 +0,0 @@
-import { InitializePayment, PaystackProps } from './types';
-type IPaystackContext = {
-    config: PaystackProps;
-    initializePayment: InitializePayment;
-    onSuccess: () => void;
-    onClose: () => void;
-};
-declare const PaystackContext: import("react").Context<IPaystackContext>;
-export default PaystackContext;
--- node_modules/react-paystack/libs/paystack-actions.ts
+++
@@ -1,6 +0,0 @@
-import PaystackPop from '@paystack/inline-js';
-
-export const callPaystackPop = (paystackArgs: Record<string, any>): void => {
-  const paystack = new PaystackPop();
-  paystack.newTransaction(paystackArgs);
-};
